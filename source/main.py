25519da8be2dbe681e413a55ffb127d37129f4b4
StarMiner99 <bijstoersma@gmail.com>
2019-02-25 20:21:28 +0100

    2.9.6

--- source/main.py
+++ source/main.py
@@ -1,11 +1,11 @@
 from modules import mainmenu
-from updater import updater
+#from updater import updater
 #import images
 import os
-up = updater()
-update = up.look()
+#up = updater()
+#update = up.look()
     
 os.chdir(os.path.realpath(__file__).replace("main.py",''))
 
 mainmenu = mainmenu()
-mainmenu.create(update)
+mainmenu.create(False)
